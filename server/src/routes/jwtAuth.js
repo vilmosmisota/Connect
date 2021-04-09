03140150090e6f4425771f68cc058aafb08cfec8
Vilmos Misota <misotavilmos@gmail.com>
2021-04-09 14:29:53 +0100

    add login route

--- server/src/routes/jwtAuth.js
+++ server/src/routes/jwtAuth.js
@@ -3,7 +3,7 @@ const pool = require("../db/index");
 const bcrypt = require("bcrypt");
 const jwtGenerator = require("../utils/jwtGenerator");
 
-
+//register route
 router.post("/register", async (req, res) => {
     try {
 
@@ -36,7 +36,38 @@ router.post("/register", async (req, res) => {
         console.log(err.message);
         res.status(500).send("Server Error");
     }
-})
+});
+
+
+
+//login route
+router.post("/login", async (req, res) => {
+    try {
+
+        //1. destructure req.body
+        const {email, password} = req.body;
+
+        //2. check if user exists if not throw an error.
+        const user = await pool.query("SELECT * FROM users WHERE user_email = $1", [email]);
+        if(user.rows.length === 0) {
+            return res.status(401).json("Password or Email is incorrect")
+        };
+
+        //3. check if entered password is equal to database password.
+        const validPassword =  await bcrypt.compare(password, user.rows[0].user_password);
+        if (!validPassword) {
+            return res.status(401).json("Password or Email is incorrect")
+        };
+
+        //4. give them the jwt token
+        const token = jwtGenerator(user.rows[0].user_id);
+        res.json({ token });
+
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).json("Server Error");
+    }
+});
 
 
 
